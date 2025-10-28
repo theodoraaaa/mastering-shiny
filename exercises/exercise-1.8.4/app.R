71c6ae2442e005d5f9dd7b87c4f33fc39f063d7e
theodoraaaa <theacommandeur@icloud.com>
2025-10-28 14:12:38 +0000

    made input reactive to avoid repetitive code

--- exercises/exercise-1.8.4/app.R
+++ exercises/exercise-1.8.4/app.R
@@ -3,23 +3,27 @@ library(shiny)
 ui <- fluidPage(
   sliderInput("x", "If x is", min = 1, max = 50, value = 30),
   sliderInput("y", "and y is", min = 1, max = 50, value = 5),
-  "then, (x * y) is", textOutput("product"),
+  "then, (x * y) is", textOutput("initial_product"),
   "and, (x * y) + 5 is", textOutput("product_plus5"),
   "and (x * y) + 10 is", textOutput("product_plus10")
 )
 
 server <- function(input, output, session) {
-  output$product <- renderText({ 
-    product <- input$x * input$y
-    product
+  
+  product <- reactive({
+    input$x*input$y
   })
+  
+  output$initial_product <- renderText({ 
+    product()
+  })
+  
   output$product_plus5 <- renderText({ 
-    product <- input$x * input$y
-    product + 5
+    product() + 5
   })
+  
   output$product_plus10 <- renderText({ 
-    product <- input$x * input$y
-    product + 10
+    product() + 10
   })
 }
 
